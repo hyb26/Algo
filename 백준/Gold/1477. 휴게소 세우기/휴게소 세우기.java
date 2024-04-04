06960cd5afc478b4e865fd35aa6f3372e46b869e
hyb26 <156384314+hyb26@users.noreply.github.com>
2024-04-04 11:02:37 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
@@ -5,72 +5,62 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int L = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
 
-        
-        int[] list = new int[N + 1];
-        if(N!=0) {
-        	st = new StringTokenizer(bf.readLine());
-            for (int i = 0; i < N; i++) {
-                list[i] = Integer.parseInt(st.nextToken());
-            }
-        }
-        
-        list[list.length - 1] = L;
+		int[] list = new int[N + 1];
+		st = new StringTokenizer(bf.readLine());
+		for (int i = 0; i < N; i++) {
+			list[i] = Integer.parseInt(st.nextToken());
+		}
+		list[list.length - 1] = L;
 
-        Arrays.sort(list);
-//        System.out.println(Arrays.toString(list));
+		Arrays.sort(list);
+		//System.out.println(Arrays.toString(list));
 
-        int[] dist = new int[N + 1];
+		int[] dist = new int[N + 1];
 
-        dist[0] = list[0];
-        
-        int right = dist[0];
-        
-        if (list.length > 1) {
-            for (int i = 1; i <= N; i++) {
-                dist[i] = list[i] - list[i - 1];
-                right = Math.max(right, dist[i]);
-            }
-        }
-//        System.out.println(Arrays.toString(dist));
+		dist[0] = list[0];
+		
+		int right = dist[0];
+		
+		if (list.length > 1) {
+			for (int i = 1; i <= N; i++) {
+				dist[i] = list[i] - list[i - 1];
+				right = Math.max(right, dist[i]);
+			}
+		}
+		//System.out.println(Arrays.toString(dist));
 
-        int left = 1;
-        int cnt, mid = 0;
-        int result = 0;
+		int left = 1;
+		int cnt, mid = 0;
+		int result = 0;
 
-        while (left <= right) {
-            cnt = 0;
-            mid = (left + right) / 2;
-            for (int i = 0; i <= N; i++) {
-                cnt += dist[i] / mid;
-                if (dist[i] % mid != 0) {
-                    cnt++;
-                }
-            }
+		while (left <= right) {
+			cnt = 0;
+			mid = (left + right) / 2;
+			for (int i = 0; i <= N; i++) {
+				cnt += dist[i] / mid;
+				if (dist[i] % mid != 0) {
+					cnt++;
+				}
+			}
+			if (cnt > N + 1 + M) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+				result = mid;
+			}
 
-            if(left == right) {
-                result = mid;
-                break;
-            }
+		}
 
-            if (cnt > N + 1 + M) {
-                left = mid + 1;
-            } else {
-                right = mid;
-                
-            }
+		System.out.println(result);
 
-        }
-
-        System.out.println(result);
-
-    }
+	}
 
 }
