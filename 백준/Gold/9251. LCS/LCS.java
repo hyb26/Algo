313725a95ed4437f67e707c1c059c1e8401abc3f
hyb26 <156384314+hyb26@users.noreply.github.com>
2024-04-19 08:17:54 +0900

    [Gold V] Title: LCS, Time: 252 ms, Memory: 22296 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -4,21 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		String str1 = sc.nextLine();
-		String str2 = sc.nextLine();
-		
-		int[] list1;
-		int[] list2;
-		
-		list1 = new int[str1.length()];
-		list2 = new int[str2.length()];
-		for (int i = 0; i < list1.length; i++) {
-			list1[i]=str1.charAt(i)-'A';
-		}
-		for (int i = 0; i < list2.length; i++) {
-			list2[i]=str2.charAt(i)-'A';
-		}
-		
+		char[] list1 = sc.nextLine().toCharArray();
+		char[] list2 = sc.nextLine().toCharArray();
+
 		int maxresult = 0;
 		int[][] templist = new int[list1.length][list2.length];
 		int tempmax = 0;
@@ -27,17 +15,15 @@ public class Main {
 			tempmax = 0;
 			for (int j = 0; j < list2.length; j++) {
 				if (i > 0 && j > 0)
-					tempmax = Math.max(tempmax, templist[i-1][j - 1]);
+					tempmax = Math.max(tempmax, templist[i - 1][j - 1]);
 				if (list1[i] == list2[j] && i == 0)
 					templist[i][j] = 1;
-				else if (list1[i] == list2[j] && i>0)
-					templist[i][j] = Math.max(tempmax + 1, templist[i-1][j]);
-				else if(i>0){
-					templist[i][j] = templist[i-1][j];
+				else if (list1[i] == list2[j] && i > 0)
+					templist[i][j] = Math.max(tempmax + 1, templist[i - 1][j]);
+				else if (i > 0) {
+					templist[i][j] = templist[i - 1][j];
 				}
-				
 				maxresult = Math.max(maxresult, templist[i][j]);
-				
 			}
 		}
 
