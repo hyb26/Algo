faf41b9fa94a3ce5aedfb56158374db6c2c2acee
YB <156384314+hyb26@users.noreply.github.com>
2024-05-09 21:40:50 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 1456 ms, Memory: 262960 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -0,0 +1,94 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+	public static class Node {
+		int v;
+		int w;
+
+		public Node(int v, int w) {
+			this.v = v;
+			this.w = w;
+		}
+	}
+
+	public static class Nodelist {
+		int v;
+		int w;
+		int dist;
+		StringBuilder list;
+
+		public Nodelist(int v, int w, int dist, StringBuilder list) {
+			this.v = v;
+			this.w = w;
+			this.dist = dist;
+			this.list = list;
+		}
+	}
+
+	static int n;
+	static int start;
+	static int end;
+	static List<List<Node>> list;
+	static int result;
+	static int resultsize;
+	static StringBuilder resultlist;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		n = sc.nextInt();
+		int m = sc.nextInt();
+
+		list = new ArrayList<>();
+		resultsize = 0;
+		resultlist = new StringBuilder();
+
+		for (int i = 0; i < n + 1; i++) {
+			list.add(new ArrayList<>());
+		}
+
+		for (int i = 0; i < m; i++) {
+			list.get(sc.nextInt()).add(new Node(sc.nextInt(), sc.nextInt()));
+		}
+
+		start = sc.nextInt();
+		end = sc.nextInt();
+
+		find();
+
+		System.out.println(result);
+		System.out.println(resultsize);
+		System.out.println(resultlist);
+
+	}
+
+	public static void find() {
+		PriorityQueue<Nodelist> pq = new PriorityQueue<>((Nodelist a, Nodelist b) -> a.w - b.w);
+		boolean[] visited = new boolean[n+1];
+		pq.add(new Nodelist(start, 0, 1, new StringBuilder()));
+		
+		while (!pq.isEmpty()) {
+			Nodelist temp = pq.poll();
+			visited[temp.v] = true;
+			temp.list.append(temp.v + " ");
+
+			if (temp.v == end) {
+				result = temp.w;
+				resultsize = temp.dist;
+				resultlist = temp.list;
+				break;
+			}
+			for (int i = 0; i < list.get(temp.v).size(); i++) {
+				if(visited[list.get(temp.v).get(i).v]) {
+					continue;
+				}
+				StringBuilder templist = new StringBuilder(temp.list);
+				pq.add(new Nodelist(list.get(temp.v).get(i).v, list.get(temp.v).get(i).w + temp.w, temp.dist+1, templist));
+			}
+		}
+	}
+
+}
