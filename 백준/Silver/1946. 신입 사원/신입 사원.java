174a2cc3324d93e34266896c5e7f49512dca80a9
YB <156384314+hyb26@users.noreply.github.com>
2024-05-10 21:18:22 +0900

    [Silver I] Title: 신입 사원, Time: 848 ms, Memory: 300464 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,16 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		
-		int t = sc.nextInt();
+		int t = Integer.parseInt(bf.readLine());
 		for(int tc = 0; tc<t; tc++) {
-			int n = sc.nextInt();
+			int n = Integer.parseInt(bf.readLine());
 			int result = n;
 			int[] list = new int[n+1];
 			for(int i = 0; i<n; i++) {
-				list[sc.nextInt()]=sc.nextInt();
+				StringTokenizer st = new StringTokenizer(bf.readLine());
+				list[Integer.parseInt(st.nextToken())]=Integer.parseInt(st.nextToken());
 			}
 			int check = n+1;
 			for(int i = 1; i<=n; i++) {
