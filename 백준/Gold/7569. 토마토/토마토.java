0d4146353f0530a7963d9f532c6b604254c7e3af
YB <156384314+hyb26@users.noreply.github.com>
2024-06-14 21:02:39 +0900

    [Gold V] Title: 토마토, Time: 1656 ms, Memory: 323484 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -0,0 +1,85 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		int h = sc.nextInt();
+		
+		int[][][] box = new int[n][m][h];
+		
+		int yet = 0;
+		int result = 0;
+		Deque<int[]> list = new ArrayDeque<int[]>();
+		
+		for(int i = 0; i<h; i++) {
+			for(int j = 0; j<n; j++) {
+				for(int k = 0; k<m; k++) {
+					box[j][k][i]=sc.nextInt();
+					if(box[j][k][i]==1) {
+						list.add(new int[] {j, k, i, 0});
+					}else if(box[j][k][i]==0) {
+						yet++;
+					}
+				}
+			}
+		}
+		
+		if(yet!=0) {
+			while(!list.isEmpty() ) {
+				int[] temp = list.poll();
+				
+				int r = temp[0];
+				int c = temp[1];
+				int y = temp[2];
+				int time = temp[3];
+				
+				result = Math.max(result, time);
+				
+				if(r-1>=0 && box[r-1][c][y]==0) {
+					yet--;
+					box[r-1][c][y]=1;
+					list.add(new int[] {r-1, c, y, time+1});
+				}
+				if(r+1<n && box[r+1][c][y]==0) {
+					yet--;
+					box[r+1][c][y]=1;
+					list.add(new int[] {r+1, c, y, time+1});
+				}
+				if(c-1>=0 && box[r][c-1][y]==0) {
+					yet--;
+					box[r][c-1][y]=1;
+					list.add(new int[] {r, c-1, y, time+1});
+				}
+				if(c+1<m && box[r][c+1][y]==0) {
+					yet--;
+					box[r][c+1][y]=1;
+					list.add(new int[] {r, c+1, y, time+1});
+				}
+				if(y-1>=0 && box[r][c][y-1]==0) {
+					yet--;
+					box[r][c][y-1]=1;
+					list.add(new int[] {r, c, y-1, time+1});
+				}
+				if(y+1<h && box[r][c][y+1]==0) {
+					yet--;
+					box[r][c][y+1]=1;
+					list.add(new int[] {r, c, y+1, time+1});
+				}
+			}
+		}
+		
+		if(yet>0) {
+			System.out.println(-1);
+		}else {
+			System.out.println(result);
+		}
+		
+		
+		
+	}
+
+}
