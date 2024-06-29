48686b63e5bc809a279a851e589dfcfca489d07b
YB <156384314+hyb26@users.noreply.github.com>
2024-06-30 01:51:56 +0900

    [Bronze IV] Title: R2, Time: 168 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int s = sc.nextInt();
+		
+		System.out.println(2*s-n );
+		
+	}
+
+}
