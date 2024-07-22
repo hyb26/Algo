4a3a8a9e51086f56fc89cba458d7a2e28ec88626
YB <156384314+hyb26@users.noreply.github.com>
2024-07-22 23:29:00 +0900

    [Bronze V] Title: 세제곱의 합, Time: 204 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/28701. 세제곱의 합/세제곱의 합.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		long temp = ((n+1)*n)/2;
+		System.out.println(temp);
+		System.out.println(temp*temp);
+		System.out.println(temp*temp);
+	}
+
+}
